b9b1889bd6e72a1119bf00f100f9f9ddd430cc0f
Andrew Zhang <ahtz@mit.edu>
2014-01-13 11:28:08 -0500

    still some errors

--- moveTo1/RobotPlayer.java
+++ moveTo1/RobotPlayer.java
@@ -4,7 +4,26 @@ package moveTo1;
 import battlecode.common.*;
 
 import java.util.*;
-
+/*channels
+ * 1 # of corners 
+ * 2 # of cow pushers
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * 10
+ * 11 corner 1 location
+ * 12 corner 2 location
+ * 21 corner 1 cowpush direction
+ * 22 corner 2 cowpush direction
+ * 31 corner 1 # of cowpushers
+ * 32 corner 2 # of cowpushers
+ * 41 corner 1 pushing data
+ * 42
+ */
 public class RobotPlayer {
 	public static RobotController rc;
 	
@@ -18,45 +37,72 @@ public class RobotPlayer {
 	public static int width;
 	
 	public static int rType;
+	public static int rType_s;
 	public static int rX;
 	public static int rY;
-	public static boolean[][] rVisited;
+	public static int[][] rVisited;
 	public static String rAction;
 	public static int rDestinationX;
 	public static int rDestinationY;
 	
+	//pasture Variables
+	public static int corner_Index;
+	public static int pastureX;
+	public static int pastureY;
+	public static int cow_push_type;
+	public static int[] push_init_loc_X = { 0,-1, 1,-1};
+	public static int[] push_init_loc_Y = {-1, 0,-1, 1};
+	public static int pastureDir;
+	public static int push_Radius = 8;
+	
+	public static int move_Outside_Count;
 	public static void run(RobotController myRC) {
 		rc = myRC;
+		
+		//set rand seed
 		rand = new Random(myRC.getRobot().getID());
 		
 		
 		width = rc.getMapWidth();
 		height = rc.getMapHeight();
 		
+		//set robot variables
 		if(rc.getType()==RobotType.SOLDIER)
 		{
-			rType = rc.senseRobotCount()%10;
-			rVisited = new boolean[width][height];
+			rType = rc.senseRobotCount()%100; //type
+			rVisited = new int[width][height]; //saving visited locations
 			for(int i=0;i<width;i++)
 			{
 				for(int j=0;j<height;j++)
 				{
-					rVisited[i][j] = false;
+					rVisited[i][j] = -1;
 				}
 			}
-			rAction = "none";
+			rAction = "init";
 		}
 
 		while(true){
 			try{
 				if(rc.getType()==RobotType.HQ){
+					//spawn soldiers
 					tryToSpawn();
 				}else if (rc.getType()==RobotType.SOLDIER){
 					rX = rc.getLocation().x;
 					rY = rc.getLocation().y;
-					if(rType <100)
+					if(rType == 1 || rType == 4)
 					{
-						pastureBot();
+						rType_s = 1;
+						pastureBot(); //find corner and build pasture
+					}
+					else if(rType == 2 || rType == 3)
+					{
+						rType_s = 2;
+						cowPushBot(); //move cows into pastures
+					}
+					else if(rType == 5 || rType == 6)
+					{
+						rType_s = 2;
+						cowPushBot(); //move cows into pastures
 					}
 				}
 			}catch(Exception e){
@@ -69,55 +115,270 @@ public class RobotPlayer {
 		//if(Clock.getRoundNum()>=lastSpawnRound+GameConstants.HQ_SPAWN_DELAY_CONSTANT_1){
 		if(rc.isActive()&&rc.canMove(dirs[0])&&rc.senseRobotCount()<GameConstants.MAX_ROBOTS){
 			rc.spawn(dirs[0]);
+			//System.out.println("readbroad: " + rc.readBroadcast(1));
 			//lastSpawnRound=Clock.getRoundNum();
 		}
 		//}
 	}
-	
-	public static void pastureBot() throws GameActionException {
+	public static void cowPushBot() throws GameActionException {
 		if(rc.isActive())
 		{
-			if(rAction.equals("none"))
+			if(rType==5)
 			{
-				rDestinationX = rand.nextInt(width);
-				rDestinationY = rand.nextInt(height);
-				/*if(rType == 2)
-				{
-					rDestinationX = 1;
-					rDestinationY = 1;
-				}
-				else if(rType == 1)
-				{
-					rDestinationX = width - 1;
-					rDestinationY = 1;
-				}*/
-				//System.out.println("type:" + rType);
-				//System.out.println("rdx: " +  rDestinationX + " rdy: " + rDestinationY);
-				rAction = "moving";
+				System.out.println("r5:" + rAction);
 			}
-			if(rAction.equals("moving"))
+			//System.out.println("raction:" + rAction);
+			if(rAction.equals("init"))
+			{
+				int current_CowPushers = rc.readBroadcast(2)+1;
+				rc.broadcast(2, current_CowPushers);
+				corner_Index = (current_CowPushers-1)/2 + 1;
+				
+				int Ncowpushers = rc.readBroadcast(30 + corner_Index);
+				cow_push_type = Ncowpushers%10;
+				rc.broadcast(30+corner_Index, Ncowpushers + 1);
+				
+				//move to random location
+				Direction moveDirection = dirs[rand.nextInt(8)];
+				if (rc.canMove(moveDirection)) {
+					rc.move(moveDirection);
+				}
+				
+				move_Outside_Count = 0;
+				if(rType==5)
+				{
+					System.out.println("wtfmate");
+					System.out.println("corner:" + corner_Index);
+				}
+				rAction = "wFC";
+			}
+			else if(rAction.equals("wFC")) // Waiting For Corner
+			{
+				int Ncorners = rc.readBroadcast(1);
+				if(rType==5)
+				{
+					System.out.println("NCorners:" + Ncorners);
+				}
+				if(Ncorners > corner_Index-1)
+				{
+					rAction = "cF";
+				}
+				else
+				{	
+					move_Outside_Count++;
+					if(move_Outside_Count<11)
+					{
+						Direction moveDirection = dirs[rand.nextInt(8)];
+						if (rc.canMove(moveDirection)) {
+							rc.move(moveDirection);
+						}
+					}
+				}
+			}
+			else if(rAction.equals("cF")) // Corner Found
+			{
+				 pastureDir = rc.readBroadcast(20 + corner_Index);
+				
+				int pasture_Loc = rc.readBroadcast(10 + corner_Index);
+				pastureX = pasture_Loc / 100;
+				pastureY = pasture_Loc % 100;
+				System.out.println("px: "+pastureX + " py: " + pastureY);
+				
+				
+				
+				
+				rAction = "moveToPushLoc";
+				rDestinationX = pastureX - 10 * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir] - 2*int_dirs_Y[pastureDir];
+				rDestinationY = pastureX - 10 * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir] + 2*int_dirs_X[pastureDir];
+				
+				System.out.println("dx: "+ rDestinationX + " dy: " + rDestinationY);
+				move(true);
+			}
+			else if(rAction.equals("moveToPushLoc"))
+			{
+				move(true);
+			}
+			else if(rAction.equals("wait"))
+			{
+				int  tempC = rc.readBroadcast(30+corner_Index);
+				int current_CowPushers = tempC/10;
+				if(current_CowPushers>1)
+				{
+					//ready to start push
+					//System.out.println("started");
+					if(current_CowPushers<10)
+					{
+						rc.broadcast(30+corner_Index, tempC - current_CowPushers*10 + 100);
+					}
+					else
+					{
+						rc.broadcast(30+corner_Index, tempC - 100);
+					}
+					
+					
+					
+					
+					int temp_Dir = rc.readBroadcast(40+corner_Index);
+					if(temp_Dir%2 == 1)
+					{
+						System.out.println("mod: " + temp_Dir+1%10);
+						if((temp_Dir+1)%10==0)
+						{
+							rDestinationX = pastureX - push_Radius * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir] - 4*int_dirs_Y[pastureDir];
+							rDestinationY = pastureY - push_Radius * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir] + 4*int_dirs_X[pastureDir];
+						}
+						else if((temp_Dir+1)%10==2)
+						{
+							rDestinationX = pastureX - push_Radius * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir] + 4*int_dirs_Y[pastureDir];
+							rDestinationY = pastureY - push_Radius * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir] - 4*int_dirs_X[pastureDir];
+						}
+						else if((temp_Dir+1)%10==4)
+						{
+							rDestinationX = pastureX - push_Radius * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir] - 8*int_dirs_Y[pastureDir];
+							rDestinationY = pastureY - push_Radius * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir] + 8*int_dirs_X[pastureDir];
+						}
+						else if((temp_Dir+1)%10==6)
+						{
+							rDestinationX = pastureX - push_Radius * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir] + 8*int_dirs_Y[pastureDir];
+							rDestinationY = pastureY - push_Radius * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir] - 8*int_dirs_X[pastureDir];
+						}
+						else if((temp_Dir+1)%10==8)
+						{
+							rDestinationX = pastureX - push_Radius * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir];
+							rDestinationY = pastureY - push_Radius * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir];
+						}
+						rAction = "return";
+					}
+					else
+					{
+						rDestinationX = pastureX - 1 * int_dirs_X[pastureDir] + push_init_loc_X[cow_push_type]*int_dirs_X[pastureDir];
+						rDestinationY = pastureY - 1 * int_dirs_X[pastureDir] + push_init_loc_Y[cow_push_type]*int_dirs_Y[pastureDir];
+						rAction = "push";
+					}
+					//move(true);
+				}
+			}
+			else if(rAction.equals("push"))
+			{
+				move(true);
+			}
+			else if(rAction.equals("return"))
 			{
 				move(false);
 			}
 		}
 	}
 	
+	
+	public static void pastureBot() throws GameActionException {
+		if(rc.isActive())
+		{
+			if(rAction.equals("init"))
+			{
+				//rDestinationX = rand.nextInt(width);
+				//rDestinationY = rand.nextInt(height);
+				
+				//set destination corner
+				if(rType == 1)
+				{
+					rDestinationX = 1;
+					rDestinationY = 1;
+				}
+				else if(rType == -1)
+				{
+					rDestinationX = width - 1;
+					rDestinationY = height - 1;
+				}
+				else if(rType == -1)
+				{
+					rDestinationX = 1;
+					rDestinationY = height - 1;
+				}
+				else if(rType == 4)
+				{
+					rDestinationX = width - 2;
+					rDestinationY = 1;
+				}
+				//System.out.println("type:" + rType);
+				//System.out.println("rdx: " +  rDestinationX + " rdy: " + rDestinationY);
+				rAction = "moving";
+			}
+			else if(rAction.equals("moving"))
+			{
+				move(false);
+			}
+			else if(rAction.equals("makePasture"))
+			{
+				rc.construct(RobotType.PASTR);
+				
+				rAction = "done";
+			}
+			
+		}
+	}
+	
 	public static void move(boolean mFlag) throws GameActionException 
 	{
+		//found destination
 		if(rDestinationX == rX && rDestinationY == rY)
 		{
 			for(int i=0;i<width;i++)
 			{
 				for(int j=0;j<height;j++)
 				{
-					rVisited[i][j] = false;
+					rVisited[i][j] = -1;
 				}
 			}
-			rAction = "makePasture";
+			
+			if(rType_s==1)
+			{
+					//found corner
+					int Ncorners = rc.readBroadcast(1) + 1;
+					rc.broadcast(1, Ncorners);
+					rc.broadcast(10 + Ncorners, rX*100 + rY);
+					
+					int tdirection = 0;
+					
+					if(rType==1)
+					{
+						tdirection = 7;
+					}
+					if(rType==4)
+					{
+						tdirection = 2;
+					}
+					System.out.println("direction:" + tdirection);
+					rc.broadcast(20+Ncorners, tdirection);
+					
+					rAction = "makePasture";
+			}
+			else if(rType_s==2)
+			{
+				if(rAction.equals("moveToPushLoc"))
+				{
+					rAction = "wait";
+					int current_CowPushers = rc.readBroadcast(30+corner_Index);
+					rc.broadcast(30+corner_Index, current_CowPushers+10);
+					System.out.println("added");
+				}
+				else if(rAction.equals("push") || rAction.equals("return"))
+				{
+					System.out.println("test1");
+					rAction = "wait";
+					int current_CowPushers = rc.readBroadcast(30+corner_Index);
+					rc.broadcast(30+corner_Index, current_CowPushers+10);
+					if(current_CowPushers+10>20)
+					{
+						System.out.println("test2");
+						int temp_dir = rc.readBroadcast(40+corner_Index);
+						rc.broadcast(40+corner_Index, temp_dir + 1);
+					}
+				}
+			}
 			return;
 		}
 		
-		
+		//find direction to destination
 		Direction moveDirection = dirs[0];
 		
 		double dx = ((double)rDestinationX) - ((double)rX);
@@ -138,7 +399,11 @@ public class RobotPlayer {
 		{
 			cur_direction -= 8;
 		}
-		
+		boolean movedFlag = false;
+		int cornerFlag = 0;
+		double cornerXSum = 0;
+		double cornerYSum = 0;
+		//find closest direction
 		for(int i=1;i<9;i++)
 		{
 			int test_direction = i;
@@ -159,8 +424,9 @@ public class RobotPlayer {
 			moveDirection = dirs[test_direction];
 			
 			if (rc.canMove(moveDirection)) {
-				if(!rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]])
+				if(rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]]==-1/* || rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]]!= test_direction*/)
 				{
+					//move!
 					if(mFlag)
 					{
 						rc.move(moveDirection);
@@ -169,15 +435,86 @@ public class RobotPlayer {
 					{
 						rc.sneak(moveDirection);
 					}
-					rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]] = true;
+					if(rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]]==-1)
+					{
+						rVisited[rX + int_dirs_X[test_direction]][rY + int_dirs_Y[test_direction]] = test_direction;
+					}
+					if(rType_s==1)
+					{
+						movedFlag = true;
+						if(cornerFlag == 0)
+						{
+							return;
+						}
+					}
+					else
+					{
+						return;
+					}
 				//	System.out.println("rx: " + rX + " ry: " + rY);
 				}
-				return;
+				
 			}
+			else
+			{
+				if(rType_s==1)
+				{
+					cornerFlag++;
+					cornerXSum += (double) int_dirs_X[test_direction];
+					cornerYSum += (double) int_dirs_Y[test_direction];
+				}
+			}
+		}
+		if(rType_s==1)
+		{
+			if(cornerFlag>2 && !movedFlag)
+			{
+				//found corner
+				int Ncorners = rc.readBroadcast(1) + 1;
+				rc.broadcast(1, Ncorners);
+				rc.broadcast(10 + Ncorners, rX*100 + rY);
+				
+				//find corner direction
+				double tdx = (double)Math.round((cornerXSum/((double)cornerFlag)));
+				double tdy = (double)(Math.round(cornerYSum/((double)cornerFlag)));
+				double tangle = Math.atan(tdy/tdx);
+				if(dx<0.0)
+				{
+					tangle += Math.PI;
+				}
+				
+				int tdirection =(int) Math.round(tangle * 4.0 / Math.PI) + 2;
+				if(tdirection<0)
+				{
+					tdirection += 8;
+				}
+				else if(tdirection>7)
+				{
+					tdirection -= 8;
+				}
+				System.out.println("direction:" + tdirection);
+				rc.broadcast(20+Ncorners, tdirection);
+				
+				rAction = "makePasture";
+			}
+		}
+		else
+		{
+		}
+		if(!movedFlag)
+		{
+			//stuck
+			for(int i=0;i<width;i++)
+			{
+				for(int j=0;j<height;j++)
+				{
+					rVisited[i][j] = -1;
+				}
+			}
+			return;
 		}
 		
 		
 		
 	}
-	
 }
